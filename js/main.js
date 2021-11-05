2b9627b87235196d00945764497e2fd28c28e754
999MeshDi666 <havkpzz@gmail.com>
2021-11-06 01:04:49 +0600

    panel responsives

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Header
+// header
 let WinBtn = document.querySelector('.win_btn');
 let menuWindows = document.querySelector('.menu_windows');
 WinBtn.addEventListener('click', ()=>{
@@ -6,16 +6,54 @@ WinBtn.addEventListener('click', ()=>{
     menuWindows.classList.toggle('menu_windows_on')
 });
 
+// menu icons action
+let AboutIcon = document.getElementById('about');
+let FAQIcon = document.getElementById('faq');
+let RulesIcon = document.getElementById('rules');
+let WinAbout = document.querySelector('.win_about');
+let WinFAQ = document.querySelector('.win_faq ');
+let WinRules = document.querySelector('.win_rules');
+
+AboutIcon.addEventListener('click',()=>{
+    WinBtn.classList.remove('win_btn_clicked');
+    menuWindows.classList.remove('menu_windows_on');
+    WinAbout.classList.add('win_about_on');
+});
+FAQIcon.addEventListener('click',()=>{
+    WinBtn.classList.remove('win_btn_clicked');
+    menuWindows.classList.remove('menu_windows_on');
+    WinFAQ.classList.add('win_faq_on');
+});
+
+RulesIcon.addEventListener('click',()=>{
+    WinBtn.classList.remove('win_btn_clicked');
+    menuWindows.classList.remove('menu_windows_on');
+    WinRules.classList.add('win_rules_on');
+});
+
+
+
+
+//timer
 function SetCurTime(){
     let CurTime = document.querySelector(".time");
     let curDate = new Date();
     if(curDate.getHours() > 11){
-        CurTime.textContent = `${curDate.getHours()}:${curDate.getMinutes()} PM`;
+        if(curDate.getMinutes() > 9){
+            CurTime.textContent = `${curDate.getHours()}:${curDate.getMinutes()} PM`;
+        }
+        else{
+            CurTime.textContent = `${curDate.getHours()}:0${curDate.getMinutes()} PM`;
+        }
     }
     else{
-        CurTime.textContent = `${curDate.getHours()}:${curDate.getMinutes()} AM`;
+        if(curDate.getMinutes() > 9){
+            CurTime.textContent = `${curDate.getHours()}:${curDate.getMinutes()} AM`;
+        }
+        else{
+            CurTime.textContent = `${curDate.getHours()}:0${curDate.getMinutes()} AM`;
+        }
     };
-
 };
 SetCurTime();
 
