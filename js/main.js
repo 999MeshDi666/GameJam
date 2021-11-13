44ab5d0e281348a303e85cc497213bdf873b46ce
999MeshDi666 <havkpzz@gmail.com>
2021-11-13 21:25:58 +0600

    Revert "Update main.js"
    This reverts commit d09c4f17c454381d92fbb9afe18529b81629218a.

--- js/main.js
+++ js/main.js
@@ -297,3 +297,18 @@ function SetCurTime(){
         }
     };
 };
+
+//icon onclick blue color
+var icons = document.querySelectorAll(".shortcut_icons")
+for (i = 0; i < icons.length; i++) {
+  icons[i].addEventListener("click", function() {
+    var actives = document.querySelectorAll(".shortcut_icons.selected");
+    var selected = "selected";
+    for(var i = 0; i < actives.length; i++) {
+      if(actives[i] !== this)
+      actives[i].classList.remove(selected)
+    }
+    this.classList.toggle(selected);
+  });
+}
+
