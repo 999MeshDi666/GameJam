eee5f0ba2a1331e0c9267afcf74c8a6a2b5f3f81
Faharet <73357375+Faharet@users.noreply.github.com>
2021-11-20 12:28:16 +0600

    mobile object move fix

--- js/mobile_obj_move.js
+++ js/mobile_obj_move.js
@@ -21,50 +21,17 @@ let TouchWinContent = document.querySelector('.win_content');
 
 
 /*Ловим касание*/
-function Drag(e){
-    e.addEventListener('touchstart', function(event) {
-        if (event.targetTouches.length == 1) {
-            document.body.style.overflow = 'hidden';
-            TouchWinContent.style.overflow = "hidden";
-            let touch = event.targetTouches[0];
-            touchOffsetX = touch.pageX - touch.target.offsetLeft;
-            touchOffsetY = touch.pageY - touch.target.offsetTop;
-        }
-    }, false);
-    /*Передвигаем объект*/
-    e.addEventListener('touchmove', function(event) {
-        if (event.targetTouches.length == 1) {
-
-            let touch = event.targetTouches[0];
-            
-            e.style.WebkitTransform="translate("+(touch.pageX-touchOffsetX)+"px,"+(touch.pageY-touchOffsetY)+"px)";
-        }
+function Drag(elem) {
+    elem.ondragstart = () => false;
+    elem.addEventListener('touchmove', function (event) {
+        elem.style.position = 'absolute';
+        var touch = event.targetTouches[0];
+        elem.style.left = touch.pageX - 50 + 'px';
+        elem.style.top = touch.pageY - 50 + 'px';
+        event.preventDefault();
     }, false);
 }
 
-Drop(TouchMoveAboutSh);
-Drop(TouchMoveFAQSh);
-Drop(TouchMoveRulesSh);
-Drop(TouchMoveScheduleSh);
-Drop(TouchMoveRegSh);
-Drop(TouchMoveAwardsSh);
-function Drop(e){
-    e.addEventListener('touchend', function(event) {
-        if (event.changedTouches.length == 1) {
-            let objWidth = e.offsetWidth;
-            let objHeight = e.offsetHeight;
-            let objX = e.offsetLeft;
-            let objY = e.offsetTop;
-            if(
-                (event.changedTouches[0].pageX > objX) &&
-                (event.changedTouches[0].pageX < (objX + objWidth)) &&
-                (event.changedTouches[0].pageY > objY) &&
-                (event.changedTouches[0].pageY < (objY + objHeight))){
-                /*Мы над объектом tarobj*/
-            }
-        }
-    }, false);
-}
 
 
 
